9ec610514801fad419fcb5144c02c37dbce9ff01
KSPSSW <kissapongs@gmail.com>
2024-09-15 17:46:04 +0700

    rename

--- src/main/java/com/mycompany/databaseproject/ConnectDatabase.java
+++ src/main/java/com/mycompany/databaseproject/ConnectDatabase.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
  *
  * @author kissa
  */
-public class DatabaseProject {
+public class ConnectDatabase {
 
     public static void main(String[] args) {
         Connection conn = null;
