1894a5907cb21b16def13879ec859a9c860edb1a
KSPSSW <kissapongs@gmail.com>
2024-09-15 17:53:13 +0700

    SelectDatabase

---
+++ src/main/java/com/mycompany/databaseproject/SelectDatabase.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.databaseproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author kissa
+ */
+public class SelectDatabase {
+    public static void main(String[] args) {
+        //connect
+        Connection conn = null;
+        String url = "jdbc:sqlite:dcoffee.db";
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connect to SQLite has been establish");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        //selection
+        String sql = "SELECT * FROM category";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getInt("category_id") + " "
+                        + rs.getString("category_name"));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        ///close
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
