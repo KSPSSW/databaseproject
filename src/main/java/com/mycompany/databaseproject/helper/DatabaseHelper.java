afc5f4d450346b46dae32ab1588cdf1f101076b1
66160264 <66160264@go.buu.ac.th>
2024-09-16 18:24:21 +0700

    dao package(complete) and create DatabaseHelper

---
+++ src/main/java/com/mycompany/databaseproject/helper/DatabaseHelper.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.databaseproject.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author informatics
+ */
+public class DatabaseHelper {
+    private static Connection conn = null;
+    private static final String URL = "jdbc:sqlite:dcoffee.db";
+    static {
+        getConnect();
+    }
+    public static synchronized Connection getConnect(){
+        if(conn == null){
+            try {
+                conn = DriverManager.getConnection(URL);
+                System.out.println("Connect to SQLite has been establish");
+            } catch (SQLException ex) {
+                Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return conn;
+    }
+    
+    public static void close() {
+        if(conn != null){
+            try {
+                conn.close();
+                conn = null;
+            } catch (SQLException ex) {
+                Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }   
+}
